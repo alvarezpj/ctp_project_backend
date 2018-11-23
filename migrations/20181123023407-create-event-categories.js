32856c89e8ce560ea5ce71f11244974b3c937bfa
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-11-23 17:57:53 -0500

    Add event_categories migration

---
+++ migrations/20181123023407-create-event-categories.js
@@ -0,0 +1,28 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('event_categories', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('event_categories');
+    }
+};
