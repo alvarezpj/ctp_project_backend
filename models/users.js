aefe6df25eed599a3699f7de5e8116ec0bf9b6db
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-11-23 20:04:42 -0500

    Add users model

---
+++ models/users.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+    const users = sequelize.define('users', {
+        first_name: {
+            allowNull: false,
+            type: DataTypes.STRING,
+        },
+        last_name: {
+            allowNull: false,
+            type: DataTypes.STRING,
+        },
+        email: {
+            allowNull: false,
+            unique: true,
+            type: DataTypes.STRING,
+        },
+        password_hash: {
+            allowNull: false,
+            type: DataTypes.STRING,
+        },
+        isActive: {
+            defaultValue: true,
+            type: DataTypes.BOOLEAN,
+        }
+    }, {});
+
+    users.associate = function(models) {
+        // associations can be defined here
+    };
+
+    return users;
+};
