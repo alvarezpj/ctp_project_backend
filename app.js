1d5448e1a7756e82e58a5ba9738c09aeeb7ad658
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-11-23 20:11:22 -0500

    Mount routers

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+// import routers
+const eventCategoriesRouter = require('./routers/eventCategories');
+const eventsRouter = require('./routers/events');
+const usersRouter = require('./routers/users');
+
 
 const app = express();
 const PORT = 8000;
@@ -7,9 +14,15 @@ const PORT = 8000;
 // log method, URL, status, and response time
 app.use(morgan('dev'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// mount routers
+app.use('/category', eventCategoriesRouter);
+app.use('/event', eventsRouter);
+app.use('/user', usersRouter);
 
-
+// catch-all route
 app.get('*', (req, res, next) => {
     res.status(200).json({ message: 'Serving requests' });
 });
