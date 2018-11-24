7528df5a9399672ba59129a0398f049a17fcd77c
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-11-23 20:05:07 -0500

    Add users migration

---
+++ migrations/20181123022930-create-users.js
@@ -0,0 +1,45 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            first_name: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            last_name: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            email: {
+                allowNull: false,
+                unique: true,
+                type: Sequelize.STRING,
+            },
+            password_hash: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            isActive: {
+                defaultValue: true,
+                type: Sequelize.BOOLEAN,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('users');
+    }
+};
