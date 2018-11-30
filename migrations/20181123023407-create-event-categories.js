9b25f7e16f996deacedbfbffc1f6e10bd39b41ea
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-11-29 19:42:03 -0500

    Remove createdAt and updatedAt columns

--- migrations/20181123023407-create-event-categories.js
+++ migrations/20181123023407-create-event-categories.js
@@ -10,14 +10,6 @@ module.exports = {
             name: {
                 allowNull: false,
                 type: Sequelize.STRING
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE
             }
         });
     },
