c74991d9b683fb37680425da863211704485c770
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-11-29 19:43:27 -0500

    Remove createdAt and updatedAt object properties

--- seeders/20181129212801-demo-event-categories.js
+++ seeders/20181129212801-demo-event-categories.js
@@ -14,50 +14,34 @@ module.exports = {
             {
                 id: 1,
                 name: 'Arts',
-                created_at: Sequelize.fn('NOW'),
-                updated_at: Sequelize.fn('NOW')
             },
             {
                 id: 2,
                 name: 'Dance',
-                created_at: Sequelize.fn('NOW'), 
-                updated_at: Sequelize.fn('NOW') 
             },
             {
                 id: 3,
                 name: 'Fashion & Beauty',
-                created_at: Sequelize.fn('NOW'),
-                updated_at: Sequelize.fn('NOW')
             },
             {
                 id: 4,
                 name: 'Film',
-                created_at: Sequelize.fn('NOW'),
-                updated_at: Sequelize.fn('NOW')
             },
             {
                 id: 5,
                 name: 'Food & Drink',
-                created_at: Sequelize.fn('NOW'),
-                updated_at: Sequelize.fn('NOW')
             },
             {
                 id: 6,
                 name: 'Language & Culture',
-                created_at: Sequelize.fn('NOW'),
-                updated_at: Sequelize.fn('NOW')
             },
             {
                 id: 7,
                 name: 'Music',
-                created_at: Sequelize.fn('NOW'),
-                updated_at: Sequelize.fn('NOW')
             },
             {
                 id: 8,
                 name: 'Outdoors & Adventure',
-                created_at: Sequelize.fn('NOW'),
-                updated_at: Sequelize.fn('NOW')
             }
         ], {});
     },
