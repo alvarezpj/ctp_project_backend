d644aa2d368f148c0ab0d87273282438a3d7aa26
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-12-02 09:04:07 -0500

    Refactor users router

--- routers/users.js
+++ routers/users.js
@@ -4,9 +4,16 @@ const controller= require('../controllers/users');
 // create router
 const router = express.Router();
 
-// set up routes
+/* set up routes */
+// create an user
 router.post('/', controller.create);
+// create an user-event association in users_events join table
+router.post('/event', controller.joinEvent);
+// retrieve all events associated to a user
+router.get('/:userId/event', controller.readAllEvents);
+// retrieve single user
 router.get('/:userId', controller.read);
+// update single user
 router.put('/:userId', controller.update);
 
 // export router
