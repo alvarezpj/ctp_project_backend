3cd4575692de48c23220894f97792b0793ee4358
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-12-02 09:05:35 -0500

    Refactor eventCategories router

--- routers/eventCategories.js
+++ routers/eventCategories.js
@@ -4,11 +4,18 @@ const controller= require('../controllers/eventCategories');
 // create router
 const router = express.Router();
 
-// set up routes
+/* set up routes */
+// create a category
 router.post('/', controller.create);
-router.get('/:categoryId', controller.read);
+// retrieve all categories
 router.get('/', controller.readAll);
+// retrieve all events associated to a category
+router.get('/:categoryId/event', controller.readAllEvents);
+// retrieve single category
+router.get('/:categoryId', controller.read);
+// update single category
 router.put('/:categoryId', controller.update);
+// delete single category
 router.delete('/:categoryId', controller.delete);
 
 // export router
