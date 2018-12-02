9b8f05f6b117689ae51f463f00bcacceabe67e95
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-12-02 09:03:26 -0500

    Add neighborhoods router

---
+++ routers/newYorkCityNeighborhoods.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const controller= require('../controllers/newYorkCityNeighborhoods');
+
+// create router
+const router = express.Router();
+
+/* set up routes */
+// retrieve all neighborhoods
+router.get('/', controller.readAll);
+// retrieve single neighborhood
+router.get('/:neighborhoodId', controller.read);
+// retrieve all events associated to a neighborhood 
+router.get('/:neighborhoodId/event', controller.readAllEvents);
+
+// export router
+module.exports = router;
