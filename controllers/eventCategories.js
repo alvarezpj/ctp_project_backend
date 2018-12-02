3e48d5e5cf133a8462714224b48a49e8d1b1968a
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-12-02 08:59:45 -0500

    Refactor middleware with associations

--- controllers/eventCategories.js
+++ controllers/eventCategories.js
@@ -1,38 +1,63 @@
 const eventCategories = require('../models').event_categories;
+const events = require('../models').events;
+const newYorkCityNeighborhoods = require('../models').new_york_city_neighborhoods;
+const users = require('../models').users;
+const { formatObject } = require('./helpers');
 
 // implement basic CRUD functionality
 const middlewares = {
+    // create category
     create(req, res, next) {
         return eventCategories
-            .create({ name: req.body.name })
-            .then(category => res.status(201).send({ 
-                id: category.id,
-                name: category.name 
-            }));
+            .create(req.body)
+            .then(category => res.status(201).json(category));
     },
-    read(req, res, next) {
-        return eventCategories
-            .findById(req.params.categoryId, { attributes: ['id', 'name'] })
-            .then(category => res.status(200).send(category));
-    },
+    // retrieve all categories
     readAll(req, res, next) {
         return eventCategories
-            .findAll({ attributes: ['id', 'name'], order: [['id', 'ASC']] })
-            .then(category => res.status(200).send(category));
+            .findAll()
+            .then(categories => res.status(200).send(categories));
     },
+    // retrieve all events associated to a single category
+    readAllEvents(req, res, next) {
+        return eventCategories
+            .findById(
+                req.params.categoryId,
+                {
+                    include: [{
+                        model: events, include: [
+                            { model: eventCategories },
+                            { model: newYorkCityNeighborhoods },
+                            { model: users, as: 'organizer' },
+                            { model: users, as: 'attending' }
+                        ]
+                    }]
+                }
+            )
+            .then(category => res.status(200).json(formatObject(category)));
+    },
+    // retrieve single category
+    read(req, res, next) {
+        return eventCategories
+            .findById(req.params.categoryId)
+            .then(category => res.status(200).json(category));
+    },
+    // update single category
     update(req, res, next) {
         return eventCategories
             .update(
-                { name: req.body.name },
-                { returning: true, where: { id: req.params.categoryId }})
-            .then(([rowsUpdated, [updatedCategory]]) => res.status(200).send({
-                id: updatedCategory.id,
-                name: updatedCategory.name
-            }));
+                req.body,
+                {
+                    returning: true,
+                    where: { id: req.params.categoryId }
+                }
+            )
+            .then(([rowsUpdated, [updatedCategory]]) => res.status(200).json(updatedCategory));
     },
+    // delete single category
     delete(req, res, next) {
         return eventCategories
-            .destroy({ where: { id: req.params.categoryId }})
+            .destroy({ where: { id: req.params.categoryId } })
             .then(() => res.status(204).send());
     }
 };
