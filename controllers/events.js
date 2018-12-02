7e96a4090f04dd61c2c55e294350543a3c0b3f60
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-12-02 08:59:33 -0500

    Refactor middleware with associations

--- controllers/events.js
+++ controllers/events.js
@@ -1,34 +1,76 @@
 const events = require('../models').events;
-
-
-const eventAttributes = ['id', 'name', 'category', 'description', 'capacity',
-    'attending', 'address', 'neighborhood', 'longitude', 'latitude', 'organizer',
-    'event_time', 'event_date', 'duration'];
+const eventCategories = require('../models').event_categories;
+const newYorkCityNeighborhoods = require('../models').new_york_city_neighborhoods;
+const users = require('../models').users;
+const { formatEvent } = require('./helpers');
 
 // implement basic CRUD functionality
 const middlewares = {
+    // create an event
     create(req, res, next) {
         return events
-            .create(req.body, { fields: eventAttributes.filter(attribute => attribute != 'attending') })
-            .then(event => res.status(201).send(event)); 
+            .create(req.body)
+            .then(event => res.status(201).json(event));
     },
-    read(req, res, next) {
-        return events
-            .findById(req.params.eventId, { attributes: eventAttributes })
-            .then(event => res.status(200).send(event));
-    },
+    // retrieve all events
     readAll(req, res, next) {
         return events
-            .findAll({ attributes: eventAttributes, order: [['id', 'ASC']] })
-            .then(event => res.status(200).send(event));
+            .findAll({
+                order: [['id', 'ASC']],
+                include: [
+                    { model: eventCategories },
+                    { model: newYorkCityNeighborhoods },
+                    { model: users, as: 'organizer' },
+                    { model: users, as: 'attending' }
+                ]
+            })
+            .then(events => {
+                res.status(200).send(events.map(event => formatEvent(event)));
+            });
     },
+    // retrieve single event
+    read(req, res, next) {
+        return events
+            .findById(
+                req.params.eventId,
+                {
+                    include: [
+                        { model: eventCategories },
+                        { model: newYorkCityNeighborhoods },
+                        { model: users, as: 'organizer' },
+                        { model: users, as: 'attending' }
+                    ]
+                }
+            )
+            .then(event => res.status(200).json(formatEvent(event)));
+    },
+    // update single event
     update(req, res, next) {
         return events
             .update(
                 req.body,
-                { returning: true, where: { id: req.params.eventId }})
-            .then(([rowsUpdated, [updatedEvent]]) => res.status(200).send(updatedEvent));
+                {
+                    returning: true,
+                    where: { id: req.params.eventId }
+                }
+            )
+            .then(([rowsUpdated, [updatedEvent]]) => {
+                return events
+                    .findById(
+                        updatedEvent.id,
+                        { include:
+                            [
+                                { model: eventCategories },
+                                { model: newYorkCityNeighborhoods },
+                                { model: users, as: 'organizer' },
+                                { model: users, as: 'attending' }
+                            ]
+                        }
+                    )
+                    .then(event => res.status(200).json(formatEvent(event)))
+            });
     },
+    // delete single event
     delete(req, res, next) {
         return events
             .destroy({ where: { id: req.params.eventId }})
